17bdf0112139d6facab27521ee6ec1c9b950028a
Argalep <argalep@quingis.com>
2018-07-06 00:25:36 +0300

    fix

--- Ultima.Package/Helpers/Zlib.cs
+++ Ultima.Package/Helpers/Zlib.cs
@@ -3,64 +3,29 @@ using System.Runtime.InteropServices;
 
 namespace Ultima.Package
 {
-	/// <summary>
-	/// Zlib error.
-	/// </summary>
-	public enum ZLibError
-	{
-		Okay			= 0,
-		StreamEnd		= 1,
-		NeedDictionary	= 2,
-		FileError		= -1,
-		StreamError		= -2,
-		DataError		= -3,
-		MemoryError		= -4,
-		BufferError		= -5,
-		VersionError	= -6,
-	}
+    /// <summary>
+    /// Zlib error.
+    /// </summary>
+    public enum ZLibError
+    {
+        Okay = 0,
+        StreamEnd = 1,
+        NeedDictionary = 2,
+        FileError = -1,
+        StreamError = -2,
+        DataError = -3,
+        MemoryError = -4,
+        BufferError = -5,
+        VersionError = -6,
+    }
 
-	/// <summary>
-	/// Compression library.
-	/// </summary>
-	public class Zlib64
-	{
-		#region Methods
-		[DllImport("zlibwapi64", EntryPoint = "zlibVersion" )]
-		private static extern string zlibVersion();
-
-		/// <summary>
-		/// Version of the library.
-		/// </summary>
-		public static string Version
-		{
-			get
-			{
-				return zlibVersion(); 
-			}
-		}
-
-		[DllImport("zlibwapi64", EntryPoint = "uncompress" )]
-		private static extern ZLibError uncompress( byte[] dest, ref int destLen, byte[] source, int sourceLen );
-
-		/// <summary>
-		/// Decompresses array of bytes.
-		/// </summary>
-		/// <param name="dest">Destination byte array.</param>
-		/// <param name="destLength">Destination length (Sets it).</param>
-		/// <param name="source">Source byte array.</param>
-		/// <param name="sourceLength">Source length.</param>
-		/// <returns>Error code.</returns>
-		public static ZLibError Decompress( byte[] dest, ref int destLength, byte[] source, int sourceLength )
-		{
-			return uncompress( dest, ref destLength, source, sourceLength );
-		}
-		#endregion
-	}
-
-    public class Zlib32
+    /// <summary>
+    /// Compression library.
+    /// </summary>
+    public class Zlib
     {
         #region Methods
-        [DllImport("zlibwapi32", EntryPoint = "zlibVersion")]
+        [DllImport("zlibwapi", EntryPoint = "zlibVersion")]
         private static extern string zlibVersion();
 
         /// <summary>
@@ -74,7 +39,7 @@ namespace Ultima.Package
             }
         }
 
-        [DllImport("zlibwapi32", EntryPoint = "uncompress")]
+        [DllImport("zlibwapi", EntryPoint = "uncompress")]
         private static extern ZLibError uncompress(byte[] dest, ref int destLen, byte[] source, int sourceLen);
 
         /// <summary>
--- Ultima.Spy/Packets/CompressedGump.cs
+++ Ultima.Spy/Packets/CompressedGump.cs
@@ -4,70 +4,56 @@ using System.Text;
 
 namespace Ultima.Spy.Packets
 {
-	[UltimaPacket( "Compressed Gump", UltimaPacketDirection.FromServer, 0xDD )]
-	public class CompressedGump : GenericGumpPacket
-	{
-		protected override void Parse( BigEndianReader reader )
-		{
-			reader.ReadByte(); // ID
-			reader.ReadInt16(); // Size
+    [UltimaPacket("Compressed Gump", UltimaPacketDirection.FromServer, 0xDD)]
+    public class CompressedGump : GenericGumpPacket
+    {
+        protected override void Parse(BigEndianReader reader)
+        {
+            reader.ReadByte(); // ID
+            reader.ReadInt16(); // Size
 
-			_Serial = reader.ReadUInt32();
-			_GumpID = reader.ReadInt32();
-			_X = reader.ReadInt32();
-			_Y = reader.ReadInt32();
+            _Serial = reader.ReadUInt32();
+            _GumpID = reader.ReadInt32();
+            _X = reader.ReadInt32();
+            _Y = reader.ReadInt32();
 
-			int compressedEntriesLength = reader.ReadInt32() - 4;
-			int decompressedEntriesLength = reader.ReadInt32();
+            int compressedEntriesLength = reader.ReadInt32() - 4;
+            int decompressedEntriesLength = reader.ReadInt32();
 
-			byte[] compressedEntries = reader.ReadBytes( compressedEntriesLength );
-			byte[] decompressedEntries = new byte[ decompressedEntriesLength ];
+            byte[] compressedEntries = reader.ReadBytes(compressedEntriesLength);
+            byte[] decompressedEntries = new byte[decompressedEntriesLength];
 
-            if (SystemInfo.IsX64)
+            Ultima.Package.Zlib.Decompress(decompressedEntries, ref decompressedEntriesLength, compressedEntries, compressedEntriesLength);
+            string entries = Encoding.ASCII.GetString(decompressedEntries);
+
+            int lineCount = reader.ReadInt32();
+            int compressedStringsLength = reader.ReadInt32() - 4;
+            int decompressedStringsLength = reader.ReadInt32();
+
+            byte[] compressedStrings = reader.ReadBytes(compressedStringsLength);
+            byte[] decompressedStrings = new byte[decompressedStringsLength];
+
+            Ultima.Package.Zlib.Decompress(decompressedStrings, ref decompressedStringsLength, compressedStrings, compressedStringsLength);
+            string strings = Encoding.ASCII.GetString(decompressedStrings);
+
+            _Text = new List<string>();
+
+            int start = 0;
+
+            for (int i = 0; i < strings.Length; i++)
             {
-                Ultima.Package.Zlib64.Decompress(decompressedEntries, ref decompressedEntriesLength, compressedEntries, compressedEntriesLength);
+                if (strings[i] == 0)
+                {
+                    if (i - start > 0)
+                        _Text.Add(strings.Substring(start, i - start));
+                    else
+                        _Text.Add(String.Empty);
+
+                    start = i + 1;
+                }
             }
-            else
-            {
-                Ultima.Package.Zlib32.Decompress(decompressedEntries, ref decompressedEntriesLength, compressedEntries, compressedEntriesLength);
-            }
-			string entries = Encoding.ASCII.GetString( decompressedEntries );
 
-			int lineCount = reader.ReadInt32();
-			int compressedStringsLength = reader.ReadInt32() - 4;
-			int decompressedStringsLength = reader.ReadInt32();
-
-			byte[] compressedStrings = reader.ReadBytes( compressedStringsLength );
-			byte[] decompressedStrings = new byte[ decompressedStringsLength ];
-
-            if (SystemInfo.IsX64)
-            {
-                Ultima.Package.Zlib64.Decompress(decompressedStrings, ref decompressedStringsLength, compressedStrings, compressedStringsLength);
-            }
-            else
-            {
-                Ultima.Package.Zlib32.Decompress(decompressedStrings, ref decompressedStringsLength, compressedStrings, compressedStringsLength);
-            }            
-			string strings = Encoding.ASCII.GetString( decompressedStrings );
-			
-			_Text = new List<string>();
-
-			int start = 0;
-
-			for ( int i = 0; i < strings.Length; i++ )
-			{
-				if ( strings[ i ] == 0 )
-				{
-					if ( i - start > 0 ) 
-						_Text.Add( strings.Substring( start, i - start ) );
-					else
-						_Text.Add( String.Empty );
-
-					start = i + 1;
-				}
-			}
-
-			Parse( entries );
-		}
-	}
+            Parse(entries);
+        }
+    }
 }
